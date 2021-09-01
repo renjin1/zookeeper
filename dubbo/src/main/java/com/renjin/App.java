f817f5b93d014a1efa2a1b10fa76468468c058f5
renjin <1075758908@qq.com>
2021-09-01 13:14:38 +0800

    hot-fix-second commit

--- dubbo/src/main/java/com/renjin/App.java
+++ dubbo/src/main/java/com/renjin/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("醉了");
+        System.out.println("醉了!!!!!!!!!!!!!!!!!");
     }
 }
